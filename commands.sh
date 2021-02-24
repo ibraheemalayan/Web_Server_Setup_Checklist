e50fbe2d33f6f6783070d725848038d6d8f86d3e
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2021-02-24 13:49:17 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+if [ `whoami` != 'root' ]
+  then
+    echo "You must be root to run this script !"
+    exit
+fi
+
+apt update
+apt upgrade -y
+apt install -y apache2 python3-pip libapache2-mod-wsgi-py3 python3-dev
