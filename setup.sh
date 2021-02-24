1db8f6024b7b412de5952ddde13568fdc5f996b8
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2021-02-24 14:15:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
 HIGHLIGHT='\033[0;33m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 if [ `whoami` != 'root' ]
   then
   
-    printf "${HIGHLIGHT}You must be root to run this script !${NC}"
+    printf "${RED}[!]${NC}${HIGHLIGHT}You must be root to run this script !${NC}"
     exit
 fi
 
 if [ -z "$1" ]
   then
-    printf "No argument supplied, usage : ${HIGHLIGHT}sudo bash setup.sh {website_name}${NC}"
+    printf "${RED}[!]${NC}No argument supplied, usage : ${HIGHLIGHT}sudo bash setup.sh {website_name}${NC}"
 fi
 
 website_name=$1
 
+printf "${RED}[-]${NC} -- ${HIGHLIGHT}updating system ...${NC}"
+printf "${RED}[<>]${NC} -- ${HIGHLIGHT}apt update${NC}"
 apt update
-apt upgrade -y
 
+printf "${RED}[-]${NC} -- ${HIGHLIGHT}upgrading system ...${NC}"
+printf "${RED}[<>]${NC} -- ${HIGHLIGHT}apt -y upgrade${NC}"
+apt -y upgrade
+
+printf "${RED}[-]${NC} -- ${HIGHLIGHT}installing required apt packages ...${NC}"
+printf "${RED}[<>]${NC} -- ${HIGHLIGHT}apt install -y apache2 python3-pip libapache2-mod-wsgi-py3 python3-dev${NC}"
 apt install -y apache2 python3-pip libapache2-mod-wsgi-py3 python3-dev
 
+printf "${RED}[-]${NC} -- ${HIGHLIGHT}enabling mod_wsgi for apache ...${NC}"
+printf "${RED}[<>]${NC} -- ${HIGHLIGHT}a2enmod wsgi${NC}"
 a2enmod wsgi
 
+
+printf "${RED}[-]${NC} -- ${HIGHLIGHT}enabling apache service...${NC}"
+printf "${RED}[<>]${NC} -- ${HIGHLIGHT}systemctl enable apache2${NC}"
 systemctl enable apache2
+
+printf "${RED}[-]${NC} -- ${HIGHLIGHT}restarting apache service...${NC}"
+printf "${RED}[<>]${NC} -- ${HIGHLIGHT}systemctl restart apache2${NC}"
 systemctl restart apache2
 
+printf "${RED}[-]${NC} -- ${HIGHLIGHT}making website directories...${NC}"
+printf "${RED}[<>]${NC} -- ${HIGHLIGHT}mkdir /var/www/$website_name${NC}"
+printf "${RED}[<>]${NC} -- ${HIGHLIGHT}mkdir /var/www/$website_name/static${NC}"
 mkdir /var/www/$website_name
+mkdir /var/www/$website_name/static
