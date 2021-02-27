adfe881cbe2e202c015ecce624a3045d34960998
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2021-02-27 10:21:48 +0200

    Update fill_configs.sh

--- fill_configs.sh
+++ fill_configs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-sed -i "s/{{{WebsiteName}}}/${WebsiteName}/" apache_site.conf
-sed -i "s/{{{DomainName}}}/${DomainName}/" apache_site.conf
-sed -i "s/{{{LimitRequestBody}}}/${LimitRequestBody}/" apache_site.conf
-sed -i "s/{{{Processes}}}/${Processes}/" apache_site.conf
-sed -i "s/{{{Threads}}}/${Threads}/" apache_site.conf
-sed -i "s/{{{StaticDirPath}}}/${StaticDirPath}/" apache_site.conf
-sed -i "s/{{{RobotsFilePath}}}/${RobotsFilePath}/" apache_site.conf
-sed -i "s/{{{FaviconPath}}}/${FaviconPath}/" apache_site.conf
-sed -i "s/{{{ServiceUser}}}/${ServiceUser}/" apache_site.conf
+sed -i "s/{{{WebsiteName}}}/${WebsiteName}/g" apache_site.conf
+sed -i "s/{{{DomainName}}}/${DomainName}/g" apache_site.conf
+sed -i "s/{{{LimitRequestBody}}}/${LimitRequestBody}/g" apache_site.conf
+sed -i "s/{{{Processes}}}/${Processes}/g" apache_site.conf
+sed -i "s/{{{Threads}}}/${Threads}/g" apache_site.conf
+sed -i "s/{{{StaticDirPath}}}/${StaticDirPath}/g" apache_site.conf
+sed -i "s/{{{RobotsFilePath}}}/${RobotsFilePath}/g" apache_site.conf
+sed -i "s/{{{FaviconPath}}}/${FaviconPath}/g" apache_site.conf
+sed -i "s/{{{ServiceUser}}}/${ServiceUser}/g" apache_site.conf
 
 cp apache_site.conf /etc/apache2/sites-available/${WebsiteName}.conf
 
-sed -i "s/{{{PathToApp}}}/${PathToApp}/" site.wsgi
-sed -i "s/{{{AppFactoryScriptName}}}/${AppFactoryScriptName}/" site.wsgi
+sed -i "s/{{{PathToApp}}}/${PathToApp}/g" site.wsgi
+sed -i "s/{{{AppFactoryScriptName}}}/${AppFactoryScriptName}/g" site.wsgi
 
 cp site.wsgi /var/www/${WebsiteName}/${WebsiteName}.wsgi
