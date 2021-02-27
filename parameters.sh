d90662ff4463bffaec2f3f1ba1a0e9b8698b9e6e
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2021-02-27 10:03:34 +0200

    Update parameters.sh

--- parameters.sh
+++ parameters.sh
@@ -1,4 +1,4 @@
-WebsiteName="name" # used only for the website folder name in /var/www and as the name of the apache config file (doesn't affect functionality)
+WebsiteName="mysitename" # used only for the website folder name in /var/www and as the name of the apache config file (doesn't affect functionality)
 DomainName="mysite.example.com" # Fully Qualified Domain Name
 HostName="mysite" # name of the server
 PublicIP="3.3.3.3"
@@ -8,6 +8,7 @@ Processes=2
 Threads=5
 
 # AppFactoryScript is the script that contains the create_app() method that returns Flask App object
+PathToRequirements="/home/user/src/requirements.txt" # the directory that contains the AppFactoryScript
 PathToApp="/home/user/src/" # the directory that contains the AppFactoryScript
 AppFactoryScriptName="init.py" # Name of the AppFactoryScript file
 
